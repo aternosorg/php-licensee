8af032d4eb6059e1074e7b89b57d2847c900e1c4
Kurt Thiemann <kurt@aternos.org>
2024-12-19 12:43:11 +0100

    generate EQUIVALENT_WORDS from spdx/license-list-XML

---
+++ src/Generated/Constants.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Aternos\Licensee\Generated;
+
+class Constants
+{
+    public const array EQUIVALENT_WORDS = [
+        "acknowledgement" => ["acknowledgment"],
+        "analog" => ["analogue"],
+        "and" => ["&"],
+        "analyze" => ["analyse"],
+        "artifact" => ["artefact"],
+        "authorization" => ["authorisation"],
+        "authorized" => ["authorised"],
+        "caliber" => ["calibre"],
+        "canceled" => ["cancelled"],
+        "capitalizations" => ["capitalisations"],
+        "catalog" => ["catalogue"],
+        "categorize" => ["categorise"],
+        "center" => ["centre"],
+        "copyright holder" => ["copyright owner"],
+        "emphasized" => ["emphasised"],
+        "favor" => ["favour"],
+        "favorite" => ["favourite"],
+        "fulfill" => ["fulfil"],
+        "fulfillment" => ["fulfilment"],
+        "initialize" => ["initialise"],
+        "judgement" => ["judgment"],
+        "labeling" => ["labelling"],
+        "labor" => ["labour"],
+        "license" => ["licence"],
+        "maximize" => ["maximise"],
+        "merchantability" => ["merchantibility"],
+        "modeled" => ["modelled"],
+        "modeling" => ["modelling"],
+        "noncommercial" => ["non-commercial"],
+        "offense" => ["offence"],
+        "optimize" => ["optimise"],
+        "organization" => ["organisation"],
+        "organize" => ["organise"],
+        "percent" => ["per cent"],
+        "practice" => ["practise"],
+        "program" => ["programme"],
+        "realize" => ["realise"],
+        "recognize" => ["recognise"],
+        "signaling" => ["signalling"],
+        "sublicense" => ["sub-license", "sub license"],
+        "utilization" => ["utilisation"],
+        "while" => ["whilst"],
+        "wilfull" => ["wilful"],
+    ];
+}
--- src/TextTransformer/SpellingTransformer.php
+++ src/TextTransformer/SpellingTransformer.php
@@ -2,60 +2,23 @@
 
 namespace Aternos\Licensee\TextTransformer;
 
+use Aternos\Licensee\Generated\Constants;
+
 class SpellingTransformer extends TextTransformer
 {
-    const array VARIETAL_WORDS = [
-        'acknowledgment' => 'acknowledgement',
-        'analogue' => 'analog',
-        'analyse' => 'analyze',
-        'artefact' => 'artifact',
-        'authorisation' => 'authorization',
-        'authorised' => 'authorized',
-        'calibre' => 'caliber',
-        'cancelled' => 'canceled',
-        'capitalisations' => 'capitalizations',
-        'catalogue' => 'catalog',
-        'categorise' => 'categorize',
-        'centre' => 'center',
-        'emphasised' => 'emphasized',
-        'favour' => 'favor',
-        'favourite' => 'favorite',
-        'fulfil' => 'fulfill',
-        'fulfilment' => 'fulfillment',
-        'initialise' => 'initialize',
-        'judgment' => 'judgement',
-        'labelling' => 'labeling',
-        'labour' => 'labor',
-        'licence' => 'license',
-        'maximise' => 'maximize',
-        'modelled' => 'modeled',
-        'modelling' => 'modeling',
-        'offence' => 'offense',
-        'optimise' => 'optimize',
-        'organisation' => 'organization',
-        'organise' => 'organize',
-        'practise' => 'practice',
-        'programme' => 'program',
-        'realise' => 'realize',
-        'recognise' => 'recognize',
-        'signalling' => 'signaling',
-        'sub-license' => 'sublicense',
-        'sub license' => 'sublicense',
-        'utilisation' => 'utilization',
-        'whilst' => 'while',
-        'wilful' => 'wilfull',
-        'non-commercial' => 'noncommercial',
-        'per cent' => 'percent',
-        'copyright owner' => 'copyright holder'
-    ];
-
     /**
      * @inheritDoc
      */
     public function transform(string $text): string
     {
-        foreach (self::VARIETAL_WORDS as $word => $replacement) {
-            $text = preg_replace("/\b" . $word . "\b/i", $replacement, $text);
+        foreach (Constants::EQUIVALENT_WORDS as $to => $from) {
+            $escapedFrom = [];
+            foreach ($from as $word) {
+                $escapedFrom[] = preg_quote($word, "/");
+            }
+
+            $pattern = "/\b(" . implode("|", $escapedFrom) . ")\b/i";
+            $text = preg_replace($pattern, $to, $text);
         }
         return $text;
     }
